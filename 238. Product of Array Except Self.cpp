46e00a50696e13c447d7184944269c8c5ab59068
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2023-11-28 14:10:32 +0530

    Update 238. Product of Array Except Self.cpp

--- 238. Product of Array Except Self.cpp
+++ 238. Product of Array Except Self.cpp
@@ -1,5 +1,6 @@
 //   238. Product of Array Except Self
 
+//  1st solution
 
 class Solution {
 public:
@@ -21,5 +22,37 @@ public:
            product =1;
        }
        return ans;
+    }
+};
+
+//  2nd solution 
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+         int n = nums.size();
+        vector<int> prefixProductFromStart(n);
+        vector<int>prefixProductFromEnd(n);
+        vector<int> ans(n);
+         
+         // compute prfixproduct        
+       prefixProductFromStart[0] = nums[0];
+        for(int i=1;i<n;i++){
+            prefixProductFromStart[i] = prefixProductFromStart[i-1] * nums[i];
+        }
+
+    // compute prfixproduct      
+    prefixProductFromEnd[n-1] = nums[n-1];
+        for(int i=n-2;i>=0;i--){
+          prefixProductFromEnd[i] = prefixProductFromEnd[i+1] * nums[i];
+        }
+        
+        // for ans array here we handl first and last element
+        ans[0] = prefixProductFromEnd[1];
+        ans[n-1] =prefixProductFromStart[n-2];
+        for(int i=1;i<n-1;i++){
+            ans[i] = prefixProductFromStart[i-1] * prefixProductFromEnd[i+1];
+        }
+        return ans;
     }
 };
